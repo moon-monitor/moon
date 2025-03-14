03408d1cefba227ef3807364b21cdfb37e19caf5
moonio <aide.moon.io@gmail.com>
2025-03-14 15:12:30 +0800

    feat(pkg): add hello package with environment and greeting functionality
    - Create env.go to manage environment variables and configuration
    - Add hello.go to print system information and logo
    - Include hello_test.go for package testing

---
+++ pkg/hello/env.go
@@ -0,0 +1,88 @@
+package hello
+
+import (
+	"os"
+	"sync"
+
+	"github.com/moon-monitor/moon/pkg/config"
+)
+
+var (
+	once  sync.Once
+	id, _ = os.Hostname()
+	env   = &Env{
+		name:     "Moon",
+		version:  "v3.0.0",
+		metadata: make(map[string]string),
+		id:       id,
+		env:      config.Environment_PROD.String(),
+	}
+)
+
+type Env struct {
+	name     string
+	version  string
+	metadata map[string]string
+	id       string
+	env      string
+}
+
+type Option func(*Env)
+
+func (e *Env) Name() string {
+	return env.name
+}
+
+func (e *Env) Version() string {
+	return env.version
+}
+
+func (e *Env) Metadata() map[string]string {
+	return env.metadata
+}
+
+func (e *Env) ID() string {
+	return env.id
+}
+
+func (e *Env) Env() string {
+	return env.env
+}
+
+func SetEnvWithOption(opts ...Option) {
+	once.Do(func() {
+		for _, opt := range opts {
+			opt(env)
+		}
+	})
+}
+
+func WithName(name string) Option {
+	return func(e *Env) {
+		e.name = name
+	}
+}
+
+func WithVersion(version string) Option {
+	return func(e *Env) {
+		e.version = version
+	}
+}
+
+func WithMetadata(metadata map[string]string) Option {
+	return func(e *Env) {
+		e.metadata = metadata
+	}
+}
+
+func WithID(id string) Option {
+	return func(e *Env) {
+		e.id = id
+	}
+}
+
+func WithEnv(env config.Environment) Option {
+	return func(e *Env) {
+		e.env = env.String()
+	}
+}
