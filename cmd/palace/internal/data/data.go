bed7d60aa243e4fe82dd22b26cf9e2b5b2f0c4bf
moonio <1058165620@qq.com>
2025-03-17 18:30:27 +0800

    refactor(data): optimize database configuration and management
    - Add Database message in conf.proto for better database configuration management
    - Update Data struct to use Database instead of config.Database for biz and alarm
    - Refactor database connection creation and management logic
    - Improve error handling and logging in database operations

--- cmd/palace/internal/data/data.go
+++ cmd/palace/internal/data/data.go
@@ -1,28 +1,30 @@
 package data
 
 import (
+	"database/sql"
 	"fmt"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
-
 	"github.com/moon-monitor/moon/cmd/palace/internal/conf"
 	"github.com/moon-monitor/moon/pkg/config"
 	"github.com/moon-monitor/moon/pkg/plugin/cache"
 	"github.com/moon-monitor/moon/pkg/plugin/email"
 	"github.com/moon-monitor/moon/pkg/plugin/gorm"
 	"github.com/moon-monitor/moon/pkg/util/safety"
+	"github.com/moon-monitor/moon/pkg/util/validate"
 )
 
 // ProviderSetData is a set of data providers.
 var ProviderSetData = wire.NewSet(New)
 
 type Data struct {
-	dataConf       *conf.Data
-	mainDB         gorm.DB
-	bizDB, eventDB *safety.Map[uint32, gorm.DB]
-	cache          cache.Cache
-	email          email.Email
+	dataConf             *conf.Data
+	mainDB               gorm.DB
+	bizDB, eventDB       *sql.DB
+	bizDBMap, eventDBMap *safety.Map[uint32, gorm.DB]
+	cache                cache.Cache
+	email                email.Email
 
 	helper *log.Helper
 }
@@ -32,14 +34,19 @@ func (d *Data) GetMainDB() gorm.DB {
 }
 
 func (d *Data) GetBizDB(teamID uint32) (gorm.DB, error) {
-	db, ok := d.bizDB.Get(teamID)
+	db, ok := d.bizDBMap.Get(teamID)
 	if !ok {
 		return db, nil
 	}
+
 	bizConf := d.dataConf.GetBiz()
+	dsn, err := d.createDatabase(bizConf, teamID, "moon_team_")
+	if err != nil {
+		return nil, err
+	}
 	c := &config.Database{
 		Driver:       bizConf.GetDriver(),
-		Dsn:          fmt.Sprintf(bizConf.GetDsn(), teamID),
+		Dsn:          dsn,
 		Debug:        bizConf.GetDebug(),
 		UseSystemLog: bizConf.GetUseSystemLog(),
 	}
@@ -47,12 +54,12 @@ func (d *Data) GetBizDB(teamID uint32) (gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	d.bizDB.Set(teamID, gormDB)
+	d.bizDBMap.Set(teamID, gormDB)
 	return d.GetBizDB(teamID)
 }
 
 func (d *Data) GetEventDB(teamID uint32) (gorm.DB, error) {
-	db, ok := d.eventDB.Get(teamID)
+	db, ok := d.eventDBMap.Get(teamID)
 	if ok {
 		return db, nil
 	}
@@ -67,8 +74,21 @@ func (d *Data) GetEventDB(teamID uint32) (gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	d.bizDB.Set(teamID, gormDB)
+	d.bizDBMap.Set(teamID, gormDB)
 	return d.GetEventDB(teamID)
+}
+
+func (d *Data) createDatabase(c *conf.Data_Database, teamID uint32, prefix string) (string, error) {
+	if !validate.TextIsNull(c.GetDsn()) {
+		return c.GetDsn(), nil
+	}
+	switch c.GetDriver() {
+	case config.Database_MYSQL:
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/", c.GetUser(), c.GetPassword(), c.GetHost(), c.GetPort())
+
+	default:
+		return "", nil
+	}
 }
 
 func (d *Data) GetCache() cache.Cache {
@@ -83,13 +103,13 @@ func (d *Data) GetEmail() email.Email {
 func New(c *conf.Bootstrap, logger log.Logger) (*Data, func(), error) {
 	var err error
 	data := &Data{
-		dataConf: c.GetData(),
-		mainDB:   nil,
-		bizDB:    safety.NewMap[uint32, gorm.DB](),
-		eventDB:  safety.NewMap[uint32, gorm.DB](),
-		cache:    nil,
-		email:    email.New(c.GetEmail()),
-		helper:   log.NewHelper(log.With(logger, "module", "data")),
+		dataConf:   c.GetData(),
+		mainDB:     nil,
+		bizDBMap:   safety.NewMap[uint32, gorm.DB](),
+		eventDBMap: safety.NewMap[uint32, gorm.DB](),
+		cache:      nil,
+		email:      email.New(c.GetEmail()),
+		helper:     log.NewHelper(log.With(logger, "module", "data")),
 	}
 
 	dataConf := c.GetData()
@@ -110,13 +130,13 @@ func New(c *conf.Bootstrap, logger log.Logger) (*Data, func(), error) {
 		if err = data.cache.Close(); err != nil {
 			data.helper.Errorw("method", "close cache", "err", err)
 		}
-		for teamID, db := range data.bizDB.List() {
+		for teamID, db := range data.bizDBMap.List() {
 			if err = db.Close(); err != nil {
 				method := fmt.Sprintf("close team [%d] biz db", teamID)
 				data.helper.Errorw("method", method, "err", err)
 			}
 		}
-		for teamID, db := range data.eventDB.List() {
+		for teamID, db := range data.eventDBMap.List() {
 			if err = db.Close(); err != nil {
 				method := fmt.Sprintf("close team [%d] alarm db", teamID)
 				data.helper.Errorw("method", method, "err", err)
