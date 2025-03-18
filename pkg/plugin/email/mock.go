f35492e82c0c681d5411021555bce5008e0f1168
moonio <1058165620@qq.com>
2025-03-18 16:31:37 +0800

    feat(email): add logging for mock email methods
    - Import log package to enable logging
    - Add debug logs for SetTo, SetSubject, SetBody, SetAttach, and SetCc methods

--- pkg/plugin/email/mock.go
+++ pkg/plugin/email/mock.go
@@ -1,5 +1,9 @@
 package email
 
+import (
+	"github.com/go-kratos/kratos/v2/log"
+)
+
 type mockEmail struct{}
 
 // Send 发送邮件
@@ -9,26 +13,31 @@ func (m *mockEmail) Send() error {
 
 // SetTo 设置收件人
 func (m *mockEmail) SetTo(to ...string) Email {
+	log.Debugw("method", "SetTo", "to", to)
 	return m
 }
 
 // SetSubject 设置邮件主题
 func (m *mockEmail) SetSubject(subject string) Email {
+	log.Debugw("method", "SetSubject", "subject", subject)
 	return m
 }
 
 // SetBody 设置邮件正文
 func (m *mockEmail) SetBody(body string, contentType ...string) Email {
+	log.Debugw("method", "SetBody", "body", body, "contentType", contentType)
 	return m
 }
 
 // SetAttach 设置附件
 func (m *mockEmail) SetAttach(attach ...string) Email {
+	log.Debugw("method", "SetAttach", "attach", attach)
 	return m
 }
 
 // SetCc 设置抄送人
 func (m *mockEmail) SetCc(cc ...string) Email {
+	log.Debugw("method", "SetCc", "cc", cc)
 	return m
 }
 
