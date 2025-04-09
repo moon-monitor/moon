b8b0a711cab7361ccef24aeb8ab8bb458328f6df
moonio <1058165620@qq.com>
2025-04-09 14:35:47 +0800

    feat(auth): add email sending functionality
    - Add email configuration to BootstrapConfig
    - Implement local and rabbitMQ-based email sending
    - Update AuthBiz to handle email sending logic
    - Add new helper functions for email sending

--- cmd/palace/internal/biz/auth.go
+++ cmd/palace/internal/biz/auth.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/moon-monitor/moon/pkg/plugin/email"
 	"golang.org/x/oauth2"
 
 	"github.com/moon-monitor/moon/cmd/palace/internal/biz/bo"
@@ -15,6 +16,7 @@ import (
 	"github.com/moon-monitor/moon/cmd/palace/internal/conf"
 	"github.com/moon-monitor/moon/cmd/palace/internal/helper/middleware"
 	"github.com/moon-monitor/moon/cmd/palace/internal/helper/permission"
+	"github.com/moon-monitor/moon/pkg/api/rabbit/common"
 	rabbitv1 "github.com/moon-monitor/moon/pkg/api/rabbit/v1"
 	"github.com/moon-monitor/moon/pkg/merr"
 	"github.com/moon-monitor/moon/pkg/util/crypto"
@@ -50,17 +52,26 @@ func NewAuthBiz(
 	rabbitRepo repository.Rabbit,
 	logger log.Logger,
 ) *AuthBiz {
+	emailConfig := bc.GetEmail()
 	return &AuthBiz{
 		bc:           bc,
 		redirectURL:  bc.GetAuth().GetOauth2().GetRedirectUri(),
 		oauthConfigs: buildOAuthConf(bc.GetAuth().GetOauth2()),
-		userRepo:     userRepo,
-		captchaRepo:  captchaRepo,
-		cacheRepo:    cacheRepo,
-		oauthRepo:    oauthRepo,
-		transaction:  transaction,
-		rabbitRepo:   rabbitRepo,
-		helper:       log.NewHelper(log.With(logger, "module", "biz.auth")),
+		emailConfig: &common.EmailConfig{
+			User:   emailConfig.GetUser(),
+			Pass:   emailConfig.GetPass(),
+			Host:   emailConfig.GetHost(),
+			Port:   emailConfig.GetPort(),
+			Enable: true,
+			Name:   emailConfig.GetName(),
+		},
+		userRepo:    userRepo,
+		captchaRepo: captchaRepo,
+		cacheRepo:   cacheRepo,
+		oauthRepo:   oauthRepo,
+		transaction: transaction,
+		rabbitRepo:  rabbitRepo,
+		helper:      log.NewHelper(log.With(logger, "module", "biz.auth")),
 	}
 }
 
@@ -68,6 +79,7 @@ type AuthBiz struct {
 	bc           *conf.Bootstrap
 	redirectURL  string
 	oauthConfigs *safety.Map[vobj.OAuthAPP, *oauth2.Config]
+	emailConfig  *common.EmailConfig
 
 	userRepo    repository.User
 	captchaRepo repository.Captcha
@@ -383,16 +395,35 @@ func (a *AuthBiz) LoginWithEmail(ctx context.Context, code string, user *system.
 func (a *AuthBiz) sendEmail(ctx context.Context, sendEmailParams *bo.SendEmailParams) error {
 	sendClient, ok := a.rabbitRepo.Send()
 	if !ok {
-		a.helper.Warnw("method", "rabbit not connected", "params", sendEmailParams)
-		return nil
+		// call local send email
+		return a.localSendEmail(ctx, sendEmailParams)
 	}
-	reply, err := sendClient.Email(ctx, &rabbitv1.SendEmailRequest{
-		Emails:      []string{sendEmailParams.Email},
-		Body:        sendEmailParams.Body,
-		ContentType: sendEmailParams.ContentType,
-		Subject:     sendEmailParams.Subject,
+	// call rabbit server send email
+	return a.rabbitSendEmail(ctx, sendClient, sendEmailParams)
+}
+
+func (a *AuthBiz) localSendEmail(_ context.Context, params *bo.SendEmailParams) error {
+	emailInstance := email.New(a.emailConfig)
+	emailInstance.SetTo(params.Email).
+		SetSubject(params.Subject).
+		SetBody(params.Body, params.ContentType)
+	if err := emailInstance.Send(); err != nil {
+		a.helper.Warnw("method", "local send email error", "params", params, "error", err)
+		return err
+	}
+	return nil
+}
+
+func (a *AuthBiz) rabbitSendEmail(ctx context.Context, client repository.SendClient, params *bo.SendEmailParams) error {
+	reply, err := client.Email(ctx, &rabbitv1.SendEmailRequest{
+		Emails:      []string{params.Email},
+		Body:        params.Body,
+		Subject:     params.Subject,
+		ContentType: params.ContentType,
+		EmailConfig: a.emailConfig,
 	})
 	if err != nil {
+		a.helper.Warnw("method", "rabbit send email error", "params", params, "error", err)
 		return err
 	}
 	a.helper.Debugf("send email reply: %v", reply)
