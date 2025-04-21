50a293b89b8f7405ebfc30bfaa85c6662be0b7ba
moonio <1058165620@qq.com>
2025-04-21 10:58:59 +0800

    feat(plugin): add mock SMS plugin and support dynamic loading
    - Add mock SMS plugin implementation for testing
    - Implement plugin loading functionality in sms package
    - Add unit tests for plugin loading and usage

---
+++ pkg/plugin/sms/mock/mock_sms_plugin.go
@@ -0,0 +1,32 @@
+//go:build plugin
+
+package main
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/moon-monitor/moon/pkg/plugin/sms"
+)
+
+type MockSender struct {
+	helper *log.Helper
+}
+
+func (m *MockSender) Send(_ context.Context, phoneNumber string, message sms.Message) error {
+	m.helper.Debugf("MockSender1.Send called with number: %s, message: %+v", phoneNumber, message)
+	return nil
+}
+
+func (m *MockSender) SendBatch(_ context.Context, phoneNumbers []string, message sms.Message) error {
+	m.helper.Debugf("MockSender1.SendBatch called with numbers: %v, message: %+v", phoneNumbers, message)
+	return nil
+}
+
+// New is the exported plugin factory function
+// Note: This must exactly match the expected signature in the main program
+func New(logger log.Logger) (sms.Sender, error) {
+	return &MockSender{
+		helper: log.NewHelper(log.With(logger, "module", "plugin.sms.mock")),
+	}, nil
+}
--- pkg/plugin/sms/sms.go
+++ pkg/plugin/sms/sms.go
@@ -1,6 +1,12 @@
 package sms
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"plugin"
+
+	"github.com/go-kratos/kratos/v2/log"
+)
 
 type Message struct {
 	TemplateParam string `json:"templateParam"`
@@ -10,4 +16,26 @@ type Message struct {
 type Sender interface {
 	Send(ctx context.Context, phoneNumber string, message Message) error
 	SendBatch(ctx context.Context, phoneNumbers []string, messages Message) error
+}
+
+// PluginNewFunc is the function signature plugins must implement
+type PluginNewFunc func(log.Logger) (Sender, error)
+
+func LoadPlugin(path string, logger log.Logger) (Sender, error) {
+	p, err := plugin.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not load plugin: %v", err)
+	}
+
+	newFuncSym, err := p.Lookup("New")
+	if err != nil {
+		return nil, fmt.Errorf("could not find New symbol: %v", err)
+	}
+
+	newFunc, ok := newFuncSym.(func(log.Logger) (Sender, error))
+	if !ok {
+		return nil, fmt.Errorf("plugin New has wrong signature")
+	}
+
+	return newFunc(logger)
 }
